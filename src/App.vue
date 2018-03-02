3222437f2e71621d31575f1651e558bff81a880b
Sergio Custodio <sergio.custodio@Digital-Grenoble.local>
2018-03-02 17:10:01 +0100

    toggle buttons and change of status added

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,64 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <greeting></greeting>
+    <!--<img src="./assets/logo.png">-->
     <h1>{{ msg }}</h1>
-    <h2>Some Links </h2>
+    <!--<h2>Some Links </h2>-->
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <coffee :name="machine.name" :status="machine.status" v-for="machine in machines"></coffee>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App Master Sergio!'
-    }
+
+  <script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        msg: 'Coffee Machines:',
+        machines: [
+          {
+            name: 'Machine1',
+            status: true
+          },
+          {
+            name: 'Machine2',
+            status: false
+          }]
+      }
+    },
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
+  h1, h2 {
+    font-weight: normal;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  li {
+    font-weight: bold;
+    margin: 10px 10px;
+    //display: inline-block;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .red {
+    color: red;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ToggleButton from 'vue-js-toggle-button'
+Vue.use(ToggleButton)
 
+Vue.component('greeting',{
+  template: '<h1> Welcome</h1>'
+});
+
+Vue.component('coffee',{
+  props: ['name', 'status'],
+  template: `<li :class="{red: status}">{{ name }} => <toggle-button v-model="status" :labels="{checked: 'On', unchecked: 'Off'}"  /></li>`
+});
 new Vue({
   el: '#app',
   render: h => h(App)
